bdd6e60b8aba67f50793e2d6ed598abf7813a722
Alvaro Iribarren <airibarren@fi.uba.ar>
2024-06-21 20:22:52 -0300

    Final exams submissions screen

---
+++ src/scenes/finalExamsSubmissions/FinalExamSubmissionsHeaderRight.tsx
@@ -0,0 +1,170 @@
+import { useNavigation } from '@react-navigation/native';
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet, Modal, TextInput, Alert, Text } from 'react-native';
+import MaterialIcon from '../../components/MaterialIcon';
+import SquaredButton from '../../components/SquaredButton';
+import { lightModeColors } from '../../styles/colorPalette';
+import { finalRepository, studentsRepository } from '../../repositories';
+import { Final, Student } from '../../models';
+import { set } from 'lodash';
+
+interface Props {
+	final: Final
+	fetchData: () => void
+}
+
+export function FinalExamSubmissionsHeaderRight({ final, fetchData }: Props) {
+	const navigation = useNavigation()
+	const [studentToAdd, setStudentToAdd] = useState<Student | null>(null)
+	
+	const [studentPadron, setStudentPadron] = useState<string>('');
+
+	const [modalPadronInputVisible, setModalPadronInputVisible] = useState(false)
+	const [confirmationModalVisible, setConfirmationModalVisible] = useState(false)
+
+	const fetchStudent = async () => {
+    try {
+      const student = await studentsRepository.getStudentByPadron(studentPadron);
+      
+			setStudentToAdd(student);
+			setStudentPadron('')
+			setModalPadronInputVisible(false)
+      setConfirmationModalVisible(true);
+    } catch (error) {
+      console.error("Failed to fetch student", error);
+      Alert.alert('Error',
+        'No pudimos buscar la información del alumno. ' +
+        'Asegurate de que el padrón esté escrito correctamente')
+    }
+  }
+
+	const cancelStudentAddition = () => {
+		setStudentPadron('');
+    setModalPadronInputVisible(false);
+  }
+
+  const confirmStudentAddition = async () => {
+    const padron: number = +studentToAdd!.padron;
+    const response = await finalRepository.addStudent(final.id, padron)
+    setStudentToAdd(null);
+    setConfirmationModalVisible(false);
+		fetchData()	
+    return response;
+  }
+
+	const addFinalExamSubmission = () => {
+		setModalPadronInputVisible(true);
+	}
+
+	return (
+		<View style={styles.navButtonsContainer}>
+			<TouchableOpacity style={styles.navButton} onPress={addFinalExamSubmission}>
+				<MaterialIcon name="plus" fontSize={24} color='gray' />
+			</TouchableOpacity>
+
+			<Modal
+				animationType="slide"
+				transparent={true}
+				visible={modalPadronInputVisible}
+				onRequestClose={() => setModalPadronInputVisible(false)}
+			>
+				<View style={styles.modalContainer}>
+					<View style={styles.modalView}>
+						<TextInput
+							style={styles.input}
+							placeholder="Ingresar padrón del estudiante"
+							value={studentPadron}
+							onChangeText={setStudentPadron}
+						/>
+						<View style={styles.buttonContainer}>
+							<SquaredButton text="Cancelar" onPress={cancelStudentAddition} color={lightModeColors.institutional} />
+							<SquaredButton text="Confirmar" onPress={fetchStudent} color={lightModeColors.institutional} />
+						</View>
+					</View>
+				</View>
+			</Modal>
+
+			<Modal
+        animationType="slide"
+        transparent={true}
+        visible={confirmationModalVisible}
+        onRequestClose={() => setConfirmationModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.confirmModalView}>
+            {studentToAdd && (
+              <>
+                <Text style={styles.studentName}>¿Está seguro de agregar este estudiante al final? </Text>
+                <Text style={{...styles.studentText, marginTop: 10 }}>Nombre completo: {studentToAdd.firstName} {studentToAdd.lastName} </Text>
+                <Text style={{...styles.studentText}}>DNI: {studentToAdd.dni}</Text>
+                <Text style={styles.studentText}>Email: {studentToAdd.email}</Text>
+                <Text style={styles.studentText}>Padron: {studentToAdd.padron}</Text>
+                <View style={{...styles.buttonContainer, marginTop: 10}}>
+                  <SquaredButton text="Cancelar" onPress={() => setConfirmationModalVisible(false)} />
+                  <SquaredButton text="Agregar" onPress={confirmStudentAddition} />
+                </View>
+              </>
+            )}
+          </View>
+        </View>
+      </Modal>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	navButtonsContainer: {
+		flexDirection: 'row',
+		marginRight: 15,
+	},
+	navButton: {
+		backgroundColor: 'transparent',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  modalView: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+    alignItems: 'center',
+  },
+	input: {
+    height: 55,
+    fontSize: 16,
+    borderRadius: 8,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+    width: '100%',
+  },
+	buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  confirmModalView: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+    alignItems: 'flex-start',
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  studentName: {
+    fontSize: 30,
+    textAlign: 'left'
+  },
+  studentText: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+});
--- src/scenes/finalExamsSubmissions/FinalExamSubmissionsListFooter.tsx
+++ src/scenes/finalExamsSubmissions/FinalExamSubmissionsListFooter.tsx
@@ -1,88 +1,31 @@
-import { View, Text, Modal, StyleSheet, Alert } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import { RoundedButton } from '../../components'
 import { finalExamSubmissions as style } from '../../styles';
-import prompt from 'react-native-prompt-android';
-import { studentsRepository } from '../../repositories';
-import { Student } from '../../models';
-import { FinalExamSubmissionStudentCard } from '../../components/FinalExamSubmissionStudentCard';
-import SquaredButton from '../../components/SquaredButton';
+import { Final, Student } from '../../models';
+import { finalRepository } from '../../repositories';
 
+interface Props {
+  final: Final
+}
 
-const FinalExamSubmissionsListFooter = () => {
-  const [showConfirmationModal, setShowConfirmationModal] = useState(false)
-  const [studentToAddManually, setStudentToAddManually] = useState<Student | null>(null)
+const FinalExamSubmissionsListFooter = ({ final }: Props) => {
 
-  const fetchStudent = async (padron: string) => {
-    try {
-      const student = await studentsRepository.getStudentByPadron(padron);
-      setStudentToAddManually(student);
-      setShowConfirmationModal(true);
-    } catch (error) {
-      console.error("Failed to fetch student", error);
-      Alert.alert('Error',
-        'No pudimos buscar la información del alumno. ' +
-        'Asegurate de que el padrón esté escrito correctamente')
-    }
-  }
-
-  const cancelStudentAddition = () => {
-    setStudentToAddManually(null);
-    setShowConfirmationModal(false);
+  const closeAct = () => {
+    console.log("Closing act");
+    return finalRepository.sendAct(final.id, 'image')
   }
 
   return (
     <View style={style().listHeaderFooter}>
-      <RoundedButton
-        text="Agregar alumno"
-        style={style().button}
-        onPress={() => prompt(
-          'Padrón del alumno',
-          '',
-          [
-            {
-              text: 'Cancelar',
-              style: 'cancel',
-            },
-            {
-              text: 'Confirmar',
-              onPress: fetchStudent
-            },
-          ],
-          {}
-        )}
-      />
 
       <View style={{ marginTop: 10 }}>
-        <RoundedButton
+        <RoundedButton 
+          onPress={closeAct}
           text="Cerrar el Acta"
           style={style().button}
         />
       </View>
-
-      <Modal
-        visible={showConfirmationModal}
-        transparent={true}
-        animationType="slide"
-      >
-        <View style={styles.modalContainer}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>
-              ¿Desea agregar el siguiente alumno a la entrega?
-            </Text>
-            <View style={{ maxHeight: 50 }}>
-              <FinalExamSubmissionStudentCard student={studentToAddManually!} />
-            </View>
-
-            <View style={styles.modalSelectorButtons}>
-              <SquaredButton text="Cancelar" onPress={cancelStudentAddition} />
-              <View style={styles.modalSelectorConfirmButton}>
-                <SquaredButton text="Confirmar" />
-              </View>
-            </View>
-          </View>
-        </View>
-      </Modal>
     </View>
   )
 }
@@ -90,71 +33,4 @@ const FinalExamSubmissionsListFooter = () => {
 export default FinalExamSubmissionsListFooter
 
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 10,
-    marginHorizontal: 12,
-  },
-  input: {
-    height: 45,
-    borderWidth: 1,
-    borderColor: '#ddd',
-    padding: 10,
-    borderRadius: 20,
-    backgroundColor: '#fff',
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 1 },
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
-    elevation: 2,
-  },
-  item: {
-    padding: 12,
-    borderBottomWidth: 1,
-    borderBottomColor: '#eee',
-    backgroundColor: 'white',
-    borderRadius: 8,
-    marginVertical: 4,
-  },
-  modalContainer: {
-    flex: 1,
-    justifyContent: 'center', // Center vertically
-    alignItems: 'center', // Center horizontally
-    backgroundColor: 'rgba(0, 0, 0, 0.5)', // Semi-transparent background for the overlay
-  },
-  modalView: {
-    width: '80%', // Set a width for the modal (adjust as needed)
-    backgroundColor: "white",
-    borderRadius: 20,
-    padding: 25,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5
-  },
-  modalText: {
-    fontSize: 16,
-    // textAlign: 'center',
-    marginBottom: 15,
-  },
-  modalSelector: {
-    width: '100%',
-    marginBottom: 15,
-    // Add any additional styling for the modal selector here
-  },
-  modalSelectorButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    marginTop: 10,
-    width: '100%',
-  },
-  modalSelectorConfirmButton: {
-    marginLeft: 10,
-  }
-});
+const styles = StyleSheet.create({});
