24dd94daabcb877010c47040a0a4f1c4c1330a1b
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2024-06-22 02:24:30 -0300

    Silence react navigation warning on logout

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ import "moment/locale/es"
 moment.locale('es');
 
 import { LogBox } from 'react-native';
-LogBox.ignoreLogs(['new NativeEventEmitter']); // To fix this we should upgrade to react-navigation 6.X AFAIK
+LogBox.ignoreLogs([
+    'new NativeEventEmitter', // To fix this we should upgrade to react-navigation 6.X AFAIK
+    'TypeError: _reactNative.Dimensions.removeEventListener is not a function (it is undefined)' // To fix this we should... probably also upgrade react-navigation
+]);
 
 AppRegistry.registerComponent(appName, () => App);
