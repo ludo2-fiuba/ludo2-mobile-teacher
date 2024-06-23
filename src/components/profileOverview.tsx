35a4269ce39f90e930eee535265bc716445b87ca
franco-giordano <31712363+franco-giordano@users.noreply.github.com>
2024-06-23 19:53:37 -0300

    Force log in when token is expired on home via ProfileOverview

--- src/components/profileOverview.tsx
+++ src/components/profileOverview.tsx
@@ -6,17 +6,20 @@ import { User } from '../models';
 import { profileOverview as style } from '../styles';
 import { SessionManager } from '../managers';
 import MaterialIcon from './MaterialIcon';
+import { makeRequest } from '../networking/makeRequest';
+import { useNavigation } from '@react-navigation/native';
 
 Icon.loadFont()
 
 export default function ProfileOverview() {
+    const navigation = useNavigation();
     const [user, setUser] = useState<User | null>(null);
     const isLoggedIn = SessionManager.getInstance()?.isLoggedIn()
 
     useEffect(() => {
         async function getUser() {
             try {
-                const fetchedUser = await usersRepository.getInfo();
+                const fetchedUser = await makeRequest(usersRepository.getInfo, navigation);
                 setUser(fetchedUser);
             }
             catch (e) {
